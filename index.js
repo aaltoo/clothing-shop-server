aa5cfb0621f14ea051b3c6a9e376d0faf36df234
y2k01 <stanportableuse@gmail.com>
2021-07-11 11:12:20 +0900

    database models

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require('express')
 require('dotenv').config()
 const sequelize = require('./db')
+const models = require('./models/models')
+const cors = require('cors')
+
 const PORT = process.env.PORT || 5000
 
 const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'Yo, it is working'})
+})
 
 const start = async () => {
     try {
         await sequelize.authenticate()
         await sequelize.sync()
-        app.listen(() => console.log(`Server is running on port ${PORT}...`))
+        app.listen(PORT,() => console.log(`Server is running on port ${PORT}...`))
     } catch (e) {
         console.log(e)
     }
