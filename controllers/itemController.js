accdfbd6a7338ccade898d814b576b662c735784
y2k01 <stanportableuse@gmail.com>
2021-07-12 10:43:22 +0900

    error middleware

---
+++ controllers/itemController.js
@@ -0,0 +1,11 @@
+class ItemController {
+    async create(req, res) {
+
+    }
+
+    async getOne(req, res) {
+
+    }
+}
+
+module.exports = new ItemController()
--- index.js
+++ index.js
@@ -3,16 +3,16 @@ require('dotenv').config()
 const sequelize = require('./db')
 const models = require('./models/models')
 const cors = require('cors')
+const router = require('./routes/index')
+const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 app.use(cors())
 app.use(express.json())
-
-app.get('/', (req, res) => {
-    res.status(200).json({message: 'Yo, it is working'})
-})
+app.use('/api', router)
+app.use(errorHandler)
 
 const start = async () => {
     try {
